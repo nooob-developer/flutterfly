4fef2f0399f7b0200c838a17d3dda344b2a6e10c
nooob-developer <nooob_developer@proton.me>
2023-07-21 21:58:38 +0330

    Add code formatting and beautification

--- noob-flutter-installer.sh
+++ noob-flutter-installer.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 read -p "Which shell do you use? (bash/zsh/etc)" user_shell
-read -p "Do you want to update system? (Y/N) " update_ans
+read -p "Do you want to update system? (y/n) " update_ans
 
 if [ -z "$update_ans" ]; then
-  update_ans="Y" 
+	update_ans="Y"
 fi
 package_manager=""
 if [[ "$update_ans" == "Y" || "$update_ans" == "y" ]]; then
-    if [ -x "$(command -v pacman)" ]; then
-    package_manager="pacman"
-    pkexec pacman -Syu
-  echo "Using $package_manager"
+	if [ -x "$(command -v pacman)" ]; then
+		package_manager="pacman"
+		pkexec pacman -Syu
+		echo "Using $package_manager"
 
-else 
- echo "Not using pacman"
+	else
+		echo "Not using pacman"
 
-  package_manager="other"
+		package_manager="other"
+	fi
+
 fi
 
-  fi
-  
 install_flutter() {
-   echo "Which AUR helper do you use? (yay/paru/etc):"
+	echo "Which AUR helper do you use? (yay/paru/etc):"
 
-read helper
+	read helper
 
-if [[ $helper =~ ^[a-zA-Z0-9]+$ ]]; then
+	if [[ $helper =~ ^[a-zA-Z0-9]+$ ]]; then
 
-helper=$(echo "$helper" | trim)
+		helper=$(echo "$helper" | trim)
 
-if ! command -v flutter &> /dev/null; then
+		if ! command -v flutter &>/dev/null; then
 
-"$helper" -S flutter
+			"$helper" -S flutter
 
-fi
-else
-echo "Invalid input. Only alphanumeric characters allowed."
-fi
+		fi
+	else
+		echo "Invalid input. Only alphanumeric characters allowed."
+	fi
 }
 
 JAVA_PATH=" 
@@ -53,60 +53,58 @@ Flutter_list="
   export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools"
 "
 
-
 if [[ "$package_manager" == *"pacman"* ]]; then
 
-  echo "Installing packages on Arch:"
+	echo "Installing packages on Arch:"
 fi
 
 if [ -d "/opt/flutter" ]; then
 
-  echo "Flutter installed successfully"
+	echo "Flutter installed successfully"
 fi
-  read -p "Do you want to install OpenJDK? (y/n)" choice
+read -p "Do you want to install OpenJDK? (y/n)" choice
 
-  if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-      
-      sudo pacman -S jdk-openjdk
-     if [ "$user_shell" == "bash" ]; then
-         echo "Add JAVA PATH"
-     echo "$JAVA_PATH" >> ~./bashrc  
- elif [ "$user_shell" == "zsh" ]; then
-     echo "Flutter_list" >> ~./zshrc
-else
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
 
-  echo "Error installing Open-Jdk"
+	sudo pacman -S jdk-openjdk
+	if [ "$user_shell" == "bash" ]; then
+		echo "Add JAVA PATH"
+		echo "$JAVA_PATH" >>~./bashrc
+	elif [ "$user_shell" == "zsh" ]; then
+		echo "Flutter_list" >>~./zshrc
+	else
 
+		echo "Error installing Open-Jdk"
+
+	fi
 fi
+read -p "Do you want add flutter PATH? (y/n)" flutter_PATH
+if [[ "$flutter_PATH" == "y" || "$flutter_PATH" == "Y" ]]; then
+
+	if [ "$user_shell" == "bash" ]; then
+		echo "Add flutter PATH to shell"
+
+		echo "$Flutter_list" >>~./bashrc
+	elif [ "$user_shell" == "zsh" ]; then
+		echo "$Flutter_list" >>~./zshrc
+
+	else
+		echo "Failed to add flutter PATH"
+	fi
 fi
-  read -p "Do you want add flutter PATH? (y/n)" flutter_PATH
-  if [[ "$flutter_PATH" == "y" || "$flutter_PATH" == "Y" ]]; then
-
-      if [ "$user_shell" == "bash" ]; then 
-           echo "Add flutter PATH to shell"
-
-    echo "$Flutter_list" >> ~./bashrc  
-  elif [ "$user_shell" == "zsh" ]; then
- echo "$Flutter_list" >> ~./zshrc  
-  
-  else
- echo "Failed to add flutter PATH"
-      fi
- fi
 
 if [ "$user_shell" == "bash" ]; then
-  source ~/.bashrc
-
+	source ~/.bashrc
 elif [ "$user_shell" == "zsh" ]; then
-  source ~/.zshrc 
+	source ~/.zshrc
 fi
 flutter doctor
 
 read -p "Do you want to see SDK file? (y/n)" see_file
 
 if [ -z "$see_file" ]; then
-see_file="Y"
+	see_file="Y"
 fi
 if [[ "$see_file" == "y" || "$see_file" == "Y" ]]; then
-cd /opt/flutter/
+	cd /opt/flutter/
 fi
