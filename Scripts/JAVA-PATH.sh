b7201a878675c40ba8baf9fafa4bacfba43b8b6d
nooob-developer <nooob_developer@proton.me>
2023-07-26 00:29:30 +0330

    Script modularization

---
+++ Scripts/JAVA-PATH.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+JAVA_PATH="
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# added by noob-flutter-script
+export JAVA_HOME=/usr/lib/jvm/java-20-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+"
+
+	read -p "Do you want to install OpenJDK? (y/n)" choice
+
+	if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+ 
+		pkexec pacman -S jdk-openjdk
+    elif [[ "$choice" == "n" || "$choice" == "N" ]]; then
+        echo "ok skip install..."
+    else
+       echo "Error invalid character" 
+    fi
+
+  PS3="Please select shell to add java-PATH: "
+    select user_shell in bash zsh; do
+      	if [ "$user_shell" == "bash" ]; then
+			echo "Add JAVA PATH"
+			echo "$JAVA_PATH" >> $HOME/.bashrc
+		elif [ "$user_shell" == "zsh" ]; then
+			echo "JAVA_PATH" >> $HOME/.zshrc
+		else
+
+			echo "Error installing Open-Jdk"
+
+		fi
+        break 
+
+    done
+
+
+
+
+
