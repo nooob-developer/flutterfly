bb4542713b04f48976b00f08b4a3d349289c5215
Nooob-developer <rbakhshi345@gmail.com>
2023-11-21 23:23:49 +0330

    fix root permission

--- Scripts/Chaotic-aur.sh
+++ Scripts/Chaotic-aur.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 pkexec pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-		sudo pacman-key --lsign-key 3056513887B78AEB
-		sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-        sudo echo "
+		pkexec pacman-key --lsign-key 3056513887B78AEB
+		pkexec pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pkexec bash -c 'echo " 
         [chaotic-aur]
-        Include = /etc/pacman.d/chaotic-mirrorlist " >> /etc/pacman.conf 
-
+        Include = /etc/pacman.d/chaotic-mirrorlist"  >> /etc/pacman.conf'
     pkexec pacman -Sy    
