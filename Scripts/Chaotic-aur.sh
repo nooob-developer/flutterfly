9986a39ba8d2ab09030ba11193201a40d9798667
Nooob-developer <rbakhshi345@gmail.com>
2024-01-21 01:56:02 +0330

    Added support only arch

--- Scripts/Chaotic-aur.sh
+++ Scripts/Chaotic-aur.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+if [ -x "$(command -v pacman)" ]; then
 pkexec pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 		pkexec pacman-key --lsign-key 3056513887B78AEB
 		pkexec pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 pkexec bash -c 'echo " 
         [chaotic-aur]
         Include = /etc/pacman.d/chaotic-mirrorlist"  >> /etc/pacman.conf'
-    pkexec pacman -Sy    
+    pkexec pacman -Sy  
+fi
