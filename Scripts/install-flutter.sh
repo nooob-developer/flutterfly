0505063876d8dd1d214f33247cbac52253593d22
Nooob-Dev <rbakhshi345@gmail.com>
2024-04-15 20:54:32 +0330

    Refactor project structure and add help function

--- Scripts/install-flutter.sh
+++ Scripts/install-flutter.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
-#set default installation PATH 
-FLUTTER_INSTALL_PATH="/opt/flutter" 
+# Set default installation path
+FLUTTER_INSTALL_PATH="/opt/flutter"
 
- AUR_method(){
+# Helper function to display help
+function help() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  aur            Install Flutter using the AUR method"
+    echo "  Chaotic-aur    Install Flutter using the Chaotic AUR method"
+    echo "  direct_download Install Flutter using the direct download method"
+    echo "  back           Go back to the main menu"
+    echo "  -h, --help     Display this help message"
+    exit 0
+}
 
-read -p "Which AUR helper do you use? (yay/paru/etc): " helper
+# Function to install Flutter using the AUR method
+aur_method() {
+    read -p "Which AUR helper do you use? (yay/paru/etc): " helper
     helper=$(echo "$helper" | sed 's/ *$//')
 
     PS3="Please select Flutter release: "
-    select release_flutter in "flutter-stable" "flutter-beta" "flutter-git" "flutter-light[Arch-Team]"; do
-        break 
+    select release_flutter in "flutter-stable" "flutter-beta" "flutter-git" "back"; do
+        case "$release_flutter" in
+            "flutter-stable")
+                aur_package="flutter"
+                ;;
+            "flutter-beta")
+                aur_package="flutter-beta"
+                ;;
+            "flutter-git")
+                aur_package="flutter-git"
+                ;;
+            "back")
+                do_arch_steps
+                ;;
+            *)
+                echo "Invalid choice"
+                exit 1
+                ;;
+        esac
+        break
     done
-
-    case "$release_flutter" in
-        "flutter-stable")
-            aur_package="flutter"
-            ;;
-        "flutter-beta")
-            aur_package="flutter-beta"
-            ;;
-        "flutter-git")
-            aur_package="flutter-git"
-            ;;
-        "flutter-light[Arch-Team]")
-            aur_package="flutter-light"
-            ;;
-        *)
-            echo "Invalid choice"
-            exit 1
-            ;;
-    esac
 
     if [ -n "$aur_package" ]; then
         $helper -S "$aur_package"
@@ -40,18 +51,18 @@ read -p "Which AUR helper do you use? (yay/paru/etc): " helper
         fi
     fi
 }
-Chaotic_method(){
-    
 
+# Function to install Flutter using the Chaotic AUR method
+chaotic_method() {
     grep -q 'chaotic' /etc/pacman.conf
     if [ $? -eq 0 ]; then
-        pkexec pacman -S flutter 
+        pkexec pacman -S flutter
         source_chaotic=$(pacman -Q | grep flutter)
         if [ -n "$source_chaotic" ]; then
             echo "INSTALL_SOURCE=Chaotic-Aur" > install-source.txt
         fi
         echo "Chaotic mirror found"
-    else 
+    else
         echo "No chaotic mirror found"
         read -p "Do you want to install and configure it? (y/n): " answer_install_chaotic
         if [[ "$answer_install_chaotic" == "y" ]]; then
@@ -66,69 +77,78 @@ Chaotic_method(){
     fi
 }
 
-Direct_download_method(){
+# Function to install Flutter using the direct download method
+direct_download_method() {
+    major_minor_version="3.19"
+    patch_version=5
 
-major_minor_version="3.16"
-patch_version=5
-update_frequency=10 # days
+    base_url="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_$major_minor_version"
+    download_url="$base_url.$patch_version-stable.tar.xz"
 
-today=$(date +%s)
-seconds_per_day=86400
-update_seconds=$((update_frequency*seconds_per_day))
+    if [ ! -d "$FLUTTER_INSTALL_PATH" ]; then
+        mkdir -p "$FLUTTER_INSTALL_PATH"
+    fi
 
-base_url="https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_$major_minor_version"
-
-if [ $((today % update_seconds)) -eq 0 ]; then
-  patch_version=$((patch_version+1))
-fi
-
-download_url="$base_url.$patch_version-stable.tar.xz"
-if [ ! -d "$FLUTTER_INSTALL_PATH" ]; then
-  mkdir -p "$FLUTTER_INSTALL_PATH"
-fi
-
-wget "$download_url" -O "/opt/flutter/flutter_linux.tar.xz"
-if [ $? -eq 0 ]; then
-    echo "Download successful"
-tar -xf "/opt/flutter/flutter_linux.tar.xz" -C "/opt/flutter"
-rm "/opt/flutter/flutter_linux.tar.xz"
-else
-    >&2 echo "Download failed"
-    read -p "Do you want to try again? (y/n): " answer_download
-    if [[ "$answer_download" == "y" ]]; then
-        echo "Retrying download..."
-        wget -c -P "$download_url" -O "/opt/flutter/flutter_linux.tar.xz"
+    wget "$download_url" -O "/opt/flutter/flutter_linux.tar.xz"
+    if [ $? -eq 0 ]; then
+        echo "Download successful"
         tar -xf "/opt/flutter/flutter_linux.tar.xz" -C "/opt/flutter"
         rm "/opt/flutter/flutter_linux.tar.xz"
+
+        # Check if the user wants to set the directory permissions to 777
+        read -p "Do you want to set the permissions of the Flutter installation directory to 777? (y/n): " set_permissions
+        if [[ "$set_permissions" == "y" ]]; then
+            chmod -R 777 "$FLUTTER_INSTALL_PATH"
+            echo "Directory permissions set to 777."
+        fi
+    else
+        >&2 echo "Download failed"
+        read -p "Do you want to try again? (y/n): " answer_download
+        if [[ "$answer_download" == "y" ]]; then
+            echo "Retrying download..."
+            wget -c -P "$download_url" -O "/opt/flutter/flutter_linux.tar.xz"
+            tar -xf "/opt/flutter/flutter_linux.tar.xz" -C "/opt/flutter"
+            rm "/opt/flutter/flutter_linux.tar.xz"
+        fi
     fi
-fi
 
-if [ -d "$FLUTTER_INSTALL_PATH" ]; then
-echo "INSTALL_SOURCE=direct_download" > install-source.txt
-fi
 
 }
 
-Do_arch_steps(){
-
-PS3="$(echo -e 'Select installation method: ')"
-select method in aur Chaotic-aur direct_download; do
-    break 
-done
-if [[ "$method" == "aur" ]]; then
-AUR_method
-fi
-
-if [[ "$method" == "Chaotic-aur" ]]; then
-Chaotic_method
-fi
-
-if [[ "$method" == "direct_download" ]]; then
-Direct_download_method
-fi 
+# Main function to select the installation method
+do_arch_steps() {
+    while true; do
+        PS3="$(echo -e 'Select installation method: ')"
+        select method in "aur" "Chaotic-aur" "direct_download" "help" "back"; do
+            case "$method" in
+                "aur")
+                    aur_method
+                    ;;
+                "Chaotic-aur")
+                    chaotic_method
+                    ;;
+                "direct_download")
+                    direct_download_method
+                    ;;
+                "help")
+                    help
+                    ;;
+                "back")
+                    return
+                    ;;
+                *)
+                    echo "Invalid choice"
+                    exit 1
+                    ;;
+            esac
+            break
+        done
+    done
 }
+
+# Check if pacman is available, and run the appropriate installation method
 if [ -x "$(command -v pacman)" ]; then
-Do_arch_steps
-else 
-    Direct_download_method
+    do_arch_steps
+else
+    direct_download_method
 fi
